de8b3fc68dc5a475a1f4a46c718a6fb49a5c285d
omar tola <mtolapacheco@gmail.com>
2021-08-05 17:42:18 -0400

    personas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('listado de personas');
+const personas = [
+    new Persona('Juan','Perez'),
+    new Persona('Karla','Lara')
+];
+
+function mostrarPersonas(){
+    console.log('mostrar personas');
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li> ${persona.nombre} ${persona.apellido}</li>`
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
